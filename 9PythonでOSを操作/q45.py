8804f012134885dbda5f1027334a9c17096bf8a0
Rikuto624 <7325562@ed.tus.ac.jp>
2025-07-23 00:26:04 +0900

    更新

--- 9PythonでOSを操作/q45.py
+++ 9PythonでOSを操作/q45.py
@@ -2,3 +2,5 @@ import os
 for corDir, dirs, files in os.walk('.'):
     for file in files:
         print(f"{corDir}/{file}")
+        print(corDir)
+        print(file)
--- 9PythonでOSを操作/q47.py
+++ 9PythonでOSを操作/q47.py
@@ -4,6 +4,7 @@ for corDir, dirs, files in os.walk('.'):
     for file in files:
         full_path = os.path.join(corDir, file)
         abs_path = os.path.abspath(full_path)
+        print(full_path)
         print(abs_path)
 
 print(os.path.abspath("./q45.py"))
